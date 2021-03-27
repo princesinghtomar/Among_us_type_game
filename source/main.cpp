8d97b401b63eb070c484683852b776170bfb2a1a
princesinghtomar <singhtomarprince470@gmail.com>
2021-03-28 00:25:42 +0530

    Started with creating ortho projection

--- source/main.cpp
+++ source/main.cpp
@@ -3,8 +3,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
-#include <list>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -12,236 +13,6 @@ void processInput(GLFWwindow *window);
 // settings
 const unsigned int SCR_WIDTH = 1280;
 const unsigned int SCR_HEIGHT = 720;
-
-#define UP 0
-#define DOWN 1
-#define RIGHT 2
-#define LEFT 3
-
-class GridBox
-{
-public:
-   bool isVisited;
-   bool pathWay[4];
-
-   GridBox()
-   {
-      isVisited = false;
-
-      for (int i = 0; i < 4; i++)
-      {
-         pathWay[i] = false;
-      }
-   }
-};
-
-std::list<float> list_xy[6];
-
-void display();
-void RenderString(float x, float y, void *font, const unsigned char *msg, float r, float g, float b);
-void display_win();
-
-GridBox *grid;
-int width, height;
-int init_x, init_y;
-int goal_x, goal_y;
-double R, G, B;
-int *chosen;
-bool isWorking;
-int state = 0;
-int userInputLastDirection = -1;
-
-GridBox &getPosXY(int x, int y)
-{
-   return grid[y * width + x];
-}
-
-void remove_line(int x, int y, int dest)
-{
-   // do remove line on the basis of coordinates
-}
-
-void update_labyrinth()
-{
-   int i;
-   int x, y;
-
-   for (i = 0; i < width * height; i++)
-   {
-      x = i % width;
-      y = i / width;
-
-      if (grid[i].pathWay[RIGHT] == true)
-      {
-         remove_line(x, y, RIGHT);
-      }
-      if (grid[i].pathWay[UP] == true)
-      {
-         remove_line(x, y, UP);
-      }
-      if (grid[i].pathWay[DOWN] == true)
-      {
-         remove_line(x, y, DOWN);
-      }
-      if (grid[i].pathWay[LEFT] == true)
-      {
-         remove_line(x, y, LEFT);
-      }
-   }
-}
-
-void choose_starting()
-{
-   int x, y;
-   int dest = rand() % 2 + 1;
-   if (dest == DOWN)
-   {
-
-      init_x = x = rand() % width;
-      init_y = y = height - 1;
-      getPosXY(x, y).pathWay[UP] = true;
-
-      goal_x = x = rand() % width;
-      goal_y = y = 0;
-      getPosXY(x, y).pathWay[DOWN] = true;
-   }
-   else
-   {
-      init_x = x = width - 1;
-      init_y = y = rand() % height;
-      getPosXY(x, y).pathWay[RIGHT] = true;
-
-      goal_x = x = 0;
-      goal_y = y = rand() % height;
-      getPosXY(x, y).pathWay[LEFT] = true;
-   }
-   chosen = new int[height * width];
-
-   x = rand() % width;
-   y = rand() % height;
-   getPosXY(x, y).isVisited = true;
-   chosen[0] = width * y + x;
-}
-
-void gen_maze()
-{
-   int x, y; // position of the current grid
-   int dest; // direction of to be connected grid
-   static int length = 0;
-   int tmp;
-
-   if (length == width * height)
-   {
-      state = 1;
-      for (int i = 0; i < width * height; i++)
-         grid[i].isVisited = false;
-      return;
-   }
-
-   if (length == 0)
-   {
-      choose_starting();
-      length = 1;
-   }
-
-   bool gridOpen = false;
-   int counter = 0;
-   while (!gridOpen)
-   {
-      tmp = chosen[rand() % length];
-      x = tmp % width;
-      y = tmp / width;
-
-      dest = rand() % 4;
-      counter++;
-      switch (dest)
-      {
-
-      case UP:
-         if (y == height - 1 || getPosXY(x, y + 1).isVisited == true)
-            continue;
-
-         getPosXY(x, y + 1).isVisited = true;
-
-         getPosXY(x, y + 1).pathWay[DOWN] = true;
-         getPosXY(x, y).pathWay[UP] = true;
-
-         chosen[length] = width * (y + 1) + x;
-         length++;
-         gridOpen = true;
-         break;
-
-      case DOWN:
-         if (y == 0 || getPosXY(x, y - 1).isVisited == true)
-            continue;
-
-         getPosXY(x, y - 1).isVisited = true;
-
-         getPosXY(x, y - 1).pathWay[UP] = true;
-         getPosXY(x, y).pathWay[DOWN] = true;
-
-         chosen[length] = width * (y - 1) + x;
-         length++;
-         gridOpen = true;
-         break;
-
-      case RIGHT:
-         if (x == width - 1 || getPosXY(x + 1, y).isVisited == true)
-            continue;
-
-         getPosXY(x + 1, y).isVisited = true;
-
-         getPosXY(x + 1, y).pathWay[LEFT] = true;
-         getPosXY(x, y).pathWay[RIGHT] = true;
-
-         chosen[length] = width * y + x + 1;
-         length++;
-         gridOpen = true;
-         break;
-
-      case LEFT:
-         if (x == 0 || getPosXY(x - 1, y).isVisited == true)
-            continue;
-
-         getPosXY(x - 1, y).isVisited = true;
-
-         getPosXY(x - 1, y).pathWay[RIGHT] = true;
-         getPosXY(x, y).pathWay[LEFT] = true;
-
-         chosen[length] = width * y + x - 1;
-         length++;
-         gridOpen = true;
-         break;
-      }
-   }
-}
-
-void input_width_and_height()
-{
-   while (1)
-   {
-      std::cout << "Input width, height (From 5 to 30)" << std::endl;
-      std::cin >> width >> height;
-      if (width > 30 || width < 5 || height > 30 || height < 5)
-      {
-         std::cout << "out of range!" << std::endl;
-      }
-      else
-      {
-         break;
-      }
-   }
-}
-
-void display_options()
-{
-   std::cout << std::endl;
-   std::cout << "Enter key : start/stop" << std::endl;
-   std::cout << "Arrow key : move the character" << std::endl;
-   std::cout << std::endl
-             << "SOLVE THE MAZE!!!" << std::endl;
-}
-
 
 GLFWwindow *initialise()
 {
@@ -276,12 +47,15 @@ GLFWwindow *initialise()
 
 const char *vertexShaderSource = "\n"
    "#version 330 core\n"
-   "layout (location = 0) in vec3 aPos;\n"
+   "layout (location = 0) in vec2 aPos;\n"
    "layout (location = 1) in vec3 aColor;\n"
    "out vec3 ourColor;\n"
+   "uniform mat4 model;\n"
+   "uniform mat4 view;\n"
+   "uniform mat4 projection;\n"
    "void main()\n"
    "{\n"
-   "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+   "  gl_Position = projection * view * model * vec4(aPos ,1.0f ,1.0f);\n"
    "  ourColor = aColor;\n"
    "}\0";
 
@@ -293,6 +67,8 @@ const char *fragmentShaderSource = "\n"
    "{\n"
    "  FragColor = vec4(ourColor, 1.0);\n"
    "}\n\0";
+
+const char *geometryShaderSource = "";
 
 static unsigned int CompileShader(unsigned int type,const char *typeShaderSource ){
    unsigned int shader = glCreateShader(type);
@@ -306,7 +82,7 @@ static unsigned int CompileShader(unsigned int type,const char *typeShaderSource
    if (!success)
    {
       glGetShaderInfoLog(shader, 512, NULL, infoLog);
-      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+      std::cout << "ERROR::SHADER::"<< type <<" COMPILATION_FAILED\n"
                 << infoLog << std::endl;
    }
    return shader;
@@ -318,8 +94,15 @@ static unsigned int CreateShader(){
    unsigned int shaderProgram = glCreateProgram();
    unsigned int vertexShader = CompileShader(GL_VERTEX_SHADER,vertexShaderSource);
    unsigned int fragmentShader = CompileShader(GL_FRAGMENT_SHADER,fragmentShaderSource);
+   unsigned int geometryShader;
+   if(geometryShaderSource != ""){
+      geometryShader =  CompileShader(GL_GEOMETRY_SHADER,geometryShaderSource);
+   }
    glAttachShader(shaderProgram, vertexShader);
    glAttachShader(shaderProgram, fragmentShader);
+   if(geometryShaderSource != ""){
+      glAttachShader(shaderProgram,geometryShader);
+   }
    glLinkProgram(shaderProgram);
 
    // check for linking errors
@@ -332,96 +115,85 @@ static unsigned int CreateShader(){
    }
    glDeleteShader(vertexShader);
    glDeleteShader(fragmentShader);
+   if(geometryShaderSource!=""){
+      glDeleteShader(geometryShader);
+   }
 
    return shaderProgram;
 }
 
 int main()
 {
-   // input_width_and_height();
-   height = width = 10;
-
-   display_options();
-
-   grid = new GridBox[width * height];
-   for (double x = 1; x < width + 2; x++)
-   {
-      list_xy[0].push_back((float)x * 10);
-      list_xy[1].push_back((float)10);
-      list_xy[2].push_back((float)0);
-      for (int i = 3; i < 6; i++)
-      {
-         list_xy[i].push_back((float)1);
-      }
-      list_xy[0].push_back((float)x * 10);
-      list_xy[1].push_back((float)height * 10 + 10.0);
-      list_xy[2].push_back((float)0);
-      for (int i = 3; i < 6; i++)
-      {
-         list_xy[i].push_back((float)1);
-      }
-      // glVertex2f(x * 10, 10.0);
-      // glVertex2f(x * 10, height * 10 + 10.0);
-   }
-   for (double x = 1; x < height + 2; x++)
-   {
-      list_xy[0].push_back((float)10);
-      list_xy[1].push_back((float)x * 10);
-      list_xy[2].push_back((float)0);
-      for (int i = 3; i < 6; i++)
-      {
-         list_xy[i].push_back((float)1);
-      }
-      list_xy[0].push_back((float)width * 10 + 10.0);
-      list_xy[1].push_back((float)x * 10);
-      list_xy[2].push_back((float)0);
-      for (int i = 3; i < 6; i++)
-      {
-         list_xy[i].push_back((float)1);
-      }
-      // glVertex2f(10.0, x * 10);
-      // glVertex2f(width * 10 + 10.0, x * 10);
-   }
-
    GLFWwindow *window = initialise();
-
+   glEnable(GL_DEPTH_TEST);  
    unsigned int shaderProgram = CreateShader();
 
    // set up vertex data (and buffer(s)) and configure vertex attributes
    // ------------------------------------------------------------------
-   
+   // float vertices[] = {
+   //    0.5f, 0.5f, 1.0f, 1.0f,0.0f,        // 0
+   //    -0.5f, 0.5f, 0.0f, 1.0f,0.0f,       // 1
+   //    0.5f, -0.5f, 1.0f, 0.0f,0.0f,       // 2
+   //    -0.5f, -0.5f, 0.0f, 0.0f,0.0f,      // 3
+   //    0.5f, 0.5f,  1.0f, 1.0f,0.0f,       // 4
+   //    -0.5f, 0.5f,  0.0f, 1.0f,0.0f,      // 5
+   //    0.5f, -0.5f,  1.0f, 0.0f,0.0f,      // 6
+   //    -0.5f, -0.5f,  0.0f, 0.0f,0.0f,     // 7
+   // };
    float vertices[] = {
-      // positions         // colors
-      0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-      -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-      0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
-   };  
+      350.0f, 350.0f, 1.0f, 1.0f,0.0f,        // 0
+      -350.0f, 350.0f, 0.0f, 1.0f,0.0f,       // 1
+      350.0f, -350.0f, 1.0f, 0.0f,0.0f,       // 2
+      -350.0f, -350.0f, 0.0f, 0.0f,0.0f,      // 3
+      350.0f, 350.0f, 1.0f, 1.0f,0.0f,       // 4
+      -350.0f, 350.0f, 0.0f, 1.0f,0.0f,      // 5
+      350.0f, -350.0f, 1.0f, 0.0f,0.0f,      // 6
+      -350.0f, -350.0f, 0.0f, 0.0f,0.0f,     // 7
+   };
+   unsigned int indices[] = {
+      7,6,4,
+      4,5,7,
+      3,2,0,
+      0,1,3,
+      1,5,7,
+      7,3,1,
+      0,4,6,
+      6,2,0,
+      7,6,2,
+      2,3,7,
+      5,4,0,
+      0,1,5
+   };
 
    unsigned int VBO, VAO;
    glGenVertexArrays(1, &VAO);
    glGenBuffers(1, &VBO);
+   // glGenBuffers(1, &EBO);
    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
    glBindVertexArray(VAO);
 
    glBindBuffer(GL_ARRAY_BUFFER, VBO);
    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6* sizeof(float), (void *)0);
+   // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+   // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
    glEnableVertexAttribArray(0);
 
-   // color attribute
-   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
-   glEnableVertexAttribArray(1);
+   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-   glBindBuffer(GL_ARRAY_BUFFER, 0);
+   // glBindBuffer(GL_ARRAY_BUFFER, 0);
 
    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-   glBindVertexArray(0);
+   // glBindVertexArray(0);
 
    // uncomment this call to draw in wireframe polygons.
-   //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
    // render loop
    // -----------
@@ -433,13 +205,26 @@ int main()
 
       // render
       // ------
-      glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-      glClear(GL_COLOR_BUFFER_BIT);
+      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
       // draw our first triangle
       glUseProgram(shaderProgram);
+      glm::mat4 model = glm::mat4(1.0f);
+      glm::mat4 view = glm::mat4(1.0f);
+      glm::mat4 projection = glm::mat4(1.0f);
+      // model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
+      view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+      // projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+      projection = glm::ortho(-(float)SCR_WIDTH/2,(float)SCR_WIDTH/2,-(float)SCR_HEIGHT/2,(float)SCR_HEIGHT/2, 0.1f, 10.0f);
+      unsigned int modelLoc = glGetUniformLocation(shaderProgram,"model");
+      unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+      glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+      glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+      glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, &projection[0][0]);
+      
       glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-      glDrawArrays(GL_TRIANGLES, 0, 3);
+      glDrawArrays(GL_TRIANGLES,0,9);
       // glBindVertexArray(0); // no need to unbind it every time
 
       // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
