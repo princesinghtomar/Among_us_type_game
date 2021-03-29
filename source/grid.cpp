5b1bc4660a0d782432caaae75c3c079026f8c5cd
princesinghtomar <singhtomarprince470@gmail.com>
2021-03-29 06:36:26 +0530

    Combined Functionality if grid and main

--- source/grid.cpp
+++ source/grid.cpp
@@ -1,4 +1,10 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <bits/stdc++.h>
+
 int height = 20;
 int width = 20;
 bool **grid;
